3e953454457568bcc22412beae5ee25f6dc8af68
Andrea Peltokorpi <aparat@Aparaatti.home>
2019-09-12 11:17:41 +0300

    Added built frontend to backend

---
+++ osa3/puhelinluettelo-backend/build/precache-manifest.8421212f671f9d8cca1d5f2022a10b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91a4c0942607e145265215eb2171ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b049a56e99fd9fa40ba3",
+    "url": "/static/css/main.eb82856b.chunk.css"
+  },
+  {
+    "revision": "404bfdd47c5cb577fdbe",
+    "url": "/static/js/2.934d4817.chunk.js"
+  },
+  {
+    "revision": "b049a56e99fd9fa40ba3",
+    "url": "/static/js/main.cc06148a.chunk.js"
+  },
+  {
+    "revision": "f66624ef4f93d72b8c01",
+    "url": "/static/js/runtime~main.8cfc7186.js"
+  }
+]);
