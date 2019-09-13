c182186b2c788e3ac2bb22ab4c6d61ec4e2fc1d4
Andrea Peltokorpi <aparat@Aparaatti.home>
2019-09-13 07:43:55 +0300

    Edited hard coded data

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -21,23 +21,23 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 
 let persons = [
   {
-    "name": "Arto Hellas",
-    "number": "040-123456",
+    "name": "Testikäyttäjä Yksi",
+    "number": "040-2383493",
     "id": 1
   },
   {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
+    "name": "Testikäyttäjä Kaksi",
+    "number": "0594093983",
     "id": 2
   },
   {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
+    "name": "Testikäyttäjä Kolme",
+    "number": "0403928930",
     "id": 3
   },
   {
-    "name": "Mary Poppendieck",
-    "number": "050-0505050",
+    "name": "Testikäyttäjä Neljä",
+    "number": "0202029384",
     "id": 4
   }
 ]
