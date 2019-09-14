8f9c746b886da7c801295553669732008db975eb
Andrea Peltokorpi <aparat@Aparaatti.home>
2019-09-14 15:03:40 +0300

    First working commit

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,44 +1,9 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://anpeltok:xtqHeje2E6h2@cluster0-f9kda.mongodb.net/mooc?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server runnning on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server runnning on port ${config.PORT}`)
 })
